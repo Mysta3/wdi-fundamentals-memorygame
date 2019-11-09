c65863c0c412a33c3d9d7ec667d6b60e6ad63e70
Myshawne <Myshawnestallings@gmail.com>
2019-11-09 12:28:30 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+var cardOne, cardTwo, cardThree, cardFour;
+
+cardOne = 'queen';
+cardTwo = 'king';
+cardThree = 'queen';
+cardFour = 'king';
+
+console.log('User flipped' + ' ' + cardFour);
