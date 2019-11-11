45815bc081256eabe0331370f4f6fab8ea9e540a
Myshawne <Myshawnestallings@gmail.com>
2019-11-11 15:02:29 -0800

    finished game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,26 +23,45 @@ var cards = [
     }
 ];
 
-
 var cardsInPlay = [];
 
+
+function createBoard(){
+    for(var i= 0; i < cards.length; i++){
+      var cardElement = document.createElement('img');
+      cardElement.setAttribute('src', 'images/back.png');
+      cardElement.setAttribute('data-id', i);  
+      cardElement.addEventListener('click', flipCard);
+      document.getElementById('game-board').appendChild(cardElement); 
+    }
+}
+
+
+
+
 function checkForMatch(){
-    if(cardsInPlay[0] === cardsInPlay[1]){
-        console.log("You found a match!")
-        }else{
-            alert("Sorry, try again.");
-        }
+        if(cardsInPlay[0] === cardsInPlay[1]){
+            alert("You found a match!")
+            }else{
+                alert("Sorry, try again.");
+            }
     }
 
-function flipCard(cardId){
+function flipCard(){
+    var cardId = this.getAttribute("data-id");
     console.log("User flipped " + cards[cardId].rank);
     console.log("User flipped " + cards[cardId].suit);
     console.log("User flipped " + cards[cardId].cardImage);
     cardsInPlay.push(cards[cardId].rank);
 
-    checkForMatch();
+    this.setAttribute('src', cards[cardId].cardImage);
+    if(cardsInPlay.length === 2){
+        checkForMatch();
+    }
 
 }
 
-flipCard(0);
-flipCard(1);
+
+createBoard();
+
+//work on a reset button
