78a7e3e8da1e287b41d72f51b6df665b0f59d855
Myshawne <Myshawnestallings@gmail.com>
2019-11-11 10:42:38 -0800

    enhanced game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
 console.log("Up and running!");
 
-var cards = ['queen', 'king', 'queen', 'king'];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"        
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"       
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"       
+    }
+];
 
 
 var cardsInPlay = [];
@@ -9,13 +30,15 @@ function checkForMatch(){
     if(cardsInPlay[0] === cardsInPlay[1]){
         console.log("You found a match!")
         }else{
-            console.log("Sorry, try again.");
+            alert("Sorry, try again.");
         }
     }
 
 function flipCard(cardId){
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log("User flipped " + cards[cardId].suit);
+    console.log("User flipped " + cards[cardId].cardImage);
+    cardsInPlay.push(cards[cardId].rank);
 
     checkForMatch();
 
